87a13ed7959442f46d46ef8950c45dd27a15bdd0
Durvish2911 <72204035+Durvish2911@users.noreply.github.com>
2020-12-20 16:51:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var momma=[]
+function id1(){console.log("moooooooo woof")};
+var papa=[];
+for(var id=1;id<=6;id++){
+    var me=document.getElementById("text_input"+id).value;
+    momma.push(me);}
+    var kemma=momma.length;
+    for( var k=0; k< kemma;k++)
+    papa.push("<h4> name-"+momma[k]+"</h4>");
+    document.getElementById("id1").innerHTML=input.join(".");
+    var me1=papa.join(" ")
+    document.getElementById("id2").innerHTML = input.join();
+    function id2(){console.log("moooooooo woof")};
+    var papa=[];
+    for(var id=1;id<=6;id++){
+        var me=document.getElementById("text_input"+id).value;
+        momma.push(me);}
+        var kemma=momma.length;
+        for( var k=0; k< kemma;k++)
+        papa.push("<h4> name-"+momma[k]+"</h4>");
+        document.getElementById("id1").innerHTML=input.join(".");
+        var me1=papa.join(" ")
+        document.getElementById("id2").innerHTML = input.join();
+    
